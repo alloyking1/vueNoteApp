f67ab4879f7664affcb3df8090936949a8c4d0e2
alloyking1 <alloyking1@gmail.com>
2023-09-04 12:02:07 +0100

    implemented a very simple note application with vueJS

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import { ref } from 'vue';
+
+  let show = ref(false);
+  let note = ref([]);
+  let noteContent = ref("");
+
+  const openCloseModal = () => {
+    noteContent.value = "";
+    show.value = !show.value;
+  }
+
+  const addNote = () => {
+    note.value.push({
+      id: Math.floor(Math.random() * 100000000),
+      text: noteContent.value,
+      date: new Date(),
+      backgroundColor: getRandomColor( )
+    });
+    openCloseModal();
+  }
+
+  const getRandomColor = () => {
+    return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+  }
+  
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="mx-[2rem] md:mx-[12rem]">
+    <div class="flex justify-between mt-12 mb-2">
+      <div class="text-4xl font-bold">Vue Note</div>
+      <div class="">
+        <button @click="openCloseModal" class="p-4 bg-black text-white rounded-full">add</button>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+    <hr>
+    <div class="grid grid-cols-4 gap-2">
+      <div class="mt-4" v-for="each in note" :key="each.id">
+        <div class="border p-2 rounded w-auto h-auto text-gray-700" :style="{backgroundColor: each.backgroundColor}">
+          <h3 class="text-sm">{{ each.text }}</h3>
+          <p class="text-sm">{{ each.date.toLocaleDateString("en-US") }}</p>
+        </div>
+      </div>
+    </div>
+    
   </main>
+  <div v-if="show" class="fixed top-0 bottom-0 left-0 right-0 p-6 bg-black opacity-75 grid grid-cols-1 place-contents-center">
+    <div class="bg-white shadow-md rounded-2xl m-32 w-auto">
+      <div class="w-auto mx-8">
+        <textarea v-model="noteContent" class="border border-gray-400 w-full mt-6 rounded-2xl p-4" cols="30" rows="10"></textarea>
+        <button @click="addNote" class="p-4 bg-blue-500 rounded w-full text-white mt-4">Add note</button>
+        <button @click="openCloseModal"  class="p-4 bg-red-500 rounded w-full text-white mt-4">Close</button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
